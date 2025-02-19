716c584e888757951c703c6218f3bc97596f8ac9
skek0 <77053845+skek0@users.noreply.github.com>
2025-02-19 21:41:42 +0900

    2025.02.19 선형보간, 카메라 오버레이, 안개효과, 싱글턴

--- Game/Assets/Scripts/Runner.cs
+++ Game/Assets/Scripts/Runner.cs
@@ -12,6 +12,7 @@ public class Runner : MonoBehaviour
     Rigidbody rigid;
     [SerializeField] float offsetX = 2.5f;
     [SerializeField] Animator animator;
+    [SerializeField] float verticalSpeed = 25.0f;
 
     private void Start()
     {
@@ -50,7 +51,15 @@ public class Runner : MonoBehaviour
     }
     public void Move()
     {
-        rigid.position = new Vector3(2.5f * (float)currentLine, 0, -6);
+        // 선형 보간법
+        // 직선에 두 점이 주어졌을 때 그 사이에 위치한 값을 추정하기 위하여
+        // 직선 거리에 따라 선형적으로 계산하는 방법
+
+        // Lerp함수의 t 값이 프레임마다 Time.DeltaTime만큼 증가하고 그에따라 대상이 부드럽게 움직
+        rigid.position = Vector3.Lerp
+            (rigid.position,
+            new Vector3(offsetX * (int)currentLine, 0, -4),
+            Time.fixedDeltaTime * verticalSpeed);
     }
 
 }
